18a579db5d58ffa9bc933ed4c2d4c85ff19f2307
Mark Anthony Espiritu <mark.a.v.espiritu@gmail.com>
2025-11-18 15:08:31 +0800

    fixing application process

--- app/Http/Controllers/Vacancies/ApplicantsController.php
+++ app/Http/Controllers/Vacancies/ApplicantsController.php
@@ -150,7 +150,7 @@ class ApplicantsController extends Controller
         }
 
         $applicant = $conn->table('applicant')
-        ->where('user_id', $application->applicant_id)
+        ->where('id', $application->applicant_id)
         ->first();
 
         if (!$applicant) {
